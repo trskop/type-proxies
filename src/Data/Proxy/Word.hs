fe908ec98afca0fc13c4528df7050bcb2e1e7eee
Peter Trško <peter.trsko@gmail.com>
2015-01-14 21:33:39 +0100

    Haddock comments for proxies

--- src/Data/Proxy/Word.hs
+++ src/Data/Proxy/Word.hs
@@ -24,17 +24,22 @@ import Data.Proxy (Proxy(Proxy))
 import Data.Word (Word, Word8, Word16, Word32, Word64)
 
 
+-- | Type proxy for 'Word'.
 word :: Proxy Word
 word = Proxy
 
+-- | Type proxy for 'Word8'.
 word8 :: Proxy Word8
 word8 = Proxy
 
+-- | Type proxy for 'Word16'.
 word16 :: Proxy Word16
 word16 = Proxy
 
+-- | Type proxy for 'Word32'.
 word32 :: Proxy Word32
 word32 = Proxy
 
+-- | Type proxy for 'Word64'.
 word64 :: Proxy Word64
 word64 = Proxy
