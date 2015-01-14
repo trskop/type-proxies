4572db385d9f9df753852ebd972556d229958789
Peter Trško <peter.trsko@gmail.com>
2015-01-14 21:30:24 +0100

    Removed dependency on text library
    As a consequence Data.String module doesn't define text, strictText and
    lazyText proxies.

--- src/Data/Proxy/String.hs
+++ src/Data/Proxy/String.hs
@@ -2,23 +2,18 @@
 -- |
 -- Module:       $HEADER$
 -- Description:  Concrete proxies for String type
--- Copyright:    (c) 2014 Peter Trsko
+-- Copyright:    (c) 2014, 2015 Peter Trsko
 -- License:      BSD3
 --
 -- Maintainer:   peter.trsko@gmail.com
 -- Stability:    experimental
 -- Portability:  NoImplicitPrelude
 --
--- Concrete proxies for 'String' type.
+-- Concrete proxies for 'String' type and .
 module Data.Proxy.String
     (
     -- * String
       string
-
-    -- * Text
-    , text
-    , strictText
-    , lazyText
 
     -- * Overloaded Strings
     , aString
@@ -30,25 +25,13 @@ import Data.Function ((.))
 import Data.Proxy (Proxy(Proxy), asProxyTypeOf)
 import Data.String (String, IsString(fromString))
 
-import qualified Data.Text as Strict (Text)
-import qualified Data.Text.Lazy as Lazy (Text)
-
 
 string :: Proxy String
 string = Proxy
 
-strictText :: Proxy Strict.Text
-strictText = Proxy
-
-lazyText :: Proxy Lazy.Text
-lazyText = Proxy
-
--- | Alias for 'strictText'.
-text :: Proxy Strict.Text
-text = strictText
-
 aString :: IsString s => Proxy s
 aString = Proxy
 
+-- | Restricted version of 'fromString'.
 fromStringTo :: IsString s => String -> Proxy s -> s
 fromStringTo = asProxyTypeOf . fromString
