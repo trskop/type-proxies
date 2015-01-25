6e41d38a738d6f53641ce1167f6b6e7e3d4613c5
Peter Trško <peter.trsko@gmail.com>
2015-01-25 13:40:12 +0100

    Type proxies for Either data type

---
+++ src/Data/Proxy/Either.hs
@@ -0,0 +1,54 @@
+{-# LANGUAGE NoImplicitPrelude #-}
+-- |
+-- Module:       $HEADER$
+-- Description:  Type proxy for Either data type.
+-- Copyright:    (c) 2015, Peter Trško
+-- License:      BSD3
+--
+-- Maintainer:   peter.trsko@gmail.com
+-- Stability:    experimental
+-- Portability:  NoImplicitPrelude
+--
+-- Type proxy for 'Either' data type.
+module Data.Proxy.Either
+    (
+    -- * Type Proxies For Either
+      anEither
+    , anEitherOf
+    , aLeftOf
+    , aRightOf
+
+    -- * Type Restricted Constructors
+    , leftOf
+    , rightOf
+    )
+  where
+
+import Data.Either (Either(Left, Right))
+import Data.Function ((.))
+import Data.Proxy (Proxy(Proxy), asProxyTypeOf)
+
+
+-- | Type proxy for 'Either'.
+anEither :: Proxy (Either a b)
+anEither = anEitherOf Proxy Proxy
+
+-- | Type proxy for 'Either' that restricts both of its type arguments.
+anEitherOf :: Proxy a -> Proxy b -> Proxy (Either a b)
+anEitherOf Proxy Proxy = Proxy
+
+-- | Type proxy for 'Either' that restricts only its left type arguments.
+aLeftOf :: Proxy a -> Proxy (Either a b)
+aLeftOf = (`anEitherOf` Proxy)
+
+-- | Type proxy for 'Either' that restricts only its right type arguments.
+aRightOf :: Proxy b -> Proxy (Either a b)
+aRightOf = anEitherOf Proxy
+
+-- | Restricted variant of 'Left' constructor of 'Either'.
+leftOf :: Proxy a -> a -> Either a b
+leftOf p = (`asProxyTypeOf` aLeftOf p) . Left
+
+-- | Restricted variant of 'Right' constructor of 'Either'.
+rightOf :: Proxy b -> b -> Either a b
+rightOf p =  (`asProxyTypeOf` aRightOf p) . Right
