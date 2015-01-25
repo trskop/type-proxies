d60e633031a96bc913f501de26f0a5a18de4b8bc
Peter Trško <peter.trsko@gmail.com>
2015-01-25 12:34:56 +0100

    Type proxies for unary, binary and ternary functions
    Type signatures are:
    
        aFunction :: Proxy (a -> b)
        aFunction2 :: Proxy (a -> b -> c)
        aFunction3 :: Proxy (a -> b -> c -> d)

--- src/Data/Proxy/Function.hs
+++ src/Data/Proxy/Function.hs
@@ -13,6 +13,11 @@
 -- Type restricted variants of functions from "Data.Function" module, and more.
 module Data.Proxy.Function
     (
+    -- * Type Proxies For Functions
+      aFunction
+    , aFunction2
+    , aFunction3
+
     -- * Restricted Versions of Standard Functions
     , idOf
     , forget
@@ -26,6 +31,21 @@ module Data.Proxy.Function
 import Data.Function (const, flip)
 import Data.Proxy (Proxy(Proxy), asProxyTypeOf)
 
+
+-- | Type proxy for unary function. Note that @b@ may be a function too. All
+-- this type proxy says is that it is at least unary function.
+aFunction :: Proxy (a -> b)
+aFunction = Proxy
+
+-- | Type proxy for binary function. Note that @b@ may be a function too. All
+-- this type proxy says is that it is at least binary function.
+aFunction2 :: Proxy (a -> b -> c)
+aFunction2 = Proxy
+
+-- | Type proxy for ternary function. Note that @b@ may be a function too. All
+-- this type proxy says is that it is at least ternary function.
+aFunction3 :: Proxy (a -> b -> c -> d)
+aFunction3 = Proxy
 
 -- | Type restricted identity function 'id' defined as:
 --
