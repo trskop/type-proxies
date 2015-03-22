b44c6d642e94607dc4df745c8f3d354a1a88aa4d
Peter Trško <peter.trsko@gmail.com>
2015-03-22 20:42:02 +0100

    Documentation for Data.Proxy.List module

--- src/Data/Proxy/List.hs
+++ src/Data/Proxy/List.hs
@@ -25,24 +25,36 @@ import Data.Maybe (Maybe(Just, Nothing))
 import Data.Proxy (Proxy(Proxy), asProxyTypeOf)
 
 
+-- | Proxy for a list with restricted type of elements.
 aListOf :: Proxy a -> Proxy [a]
 aListOf Proxy = Proxy
 
+-- | Proxy for a list without any restriction on the type of elements.
 aList :: Proxy [a]
 aList = aListOf Proxy
 
+-- | Identity function on lists that puts additional restriction on element
+-- type.
 listOf :: Proxy a -> [a] -> [a]
 listOf p = (`asProxyTypeOf` aListOf p)
 
+-- | Type restricted variant of list constructor ':'.
 consOf :: Proxy a -> a -> [a] -> [a]
 consOf p x xs = x : xs `asProxyTypeOf` aListOf p
 
+-- | Splitting list in to its head (first element) and tail (rest of the list
+-- without first element). This function also restrict type of list element.
+--
+-- @
+-- 'unconsOf' 'Data.Proxy.Word.word8' [] === Nothing :: Maybe Word8
+-- @
 unconsOf :: Proxy a -> [a] -> Maybe (a, [a])
 unconsOf p xs = uncons (xs `asProxyTypeOf` aListOf p)
   where
     uncons []       = Nothing
     uncons (y : ys) = Just (y, ys)
 
+-- | Save variant of 'Prelude.head' with type restriction on element type.
 headOf :: Proxy a -> [a] -> Maybe a
 headOf p xs = case unconsOf p xs of
     Nothing     -> Nothing
